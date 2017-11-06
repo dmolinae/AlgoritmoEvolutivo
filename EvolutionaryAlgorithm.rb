aa49accebbba97d18269be909809d128d4a10512
Daniel Molina <dmolinae@outlook.com>
2017-11-06 20:50:52 -0300

    ADD: select parents

--- EvolutionaryAlgorithm.rb
+++ EvolutionaryAlgorithm.rb
@@ -2,3 +2,11 @@ require_relative "Classes"
 
 population = Population.new.load("population.txt")
 puts population.solutions[0].fitness
+
+pc = 0.25
+number_parents = Integer(pc * population.solutions.length)
+parents = []
+number_parents.times do
+  parents.push(population.solutions[rand(0..population.solutions.length-1)])
+end
+
